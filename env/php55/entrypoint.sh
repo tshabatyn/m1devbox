e446877be0a397760615b9fd3173f87e2fca339a
Taras Shabatyn <tshabatyn@magento.com>
2016-10-07 14:55:18 +0300

    Tune nginx and php-fpm to execute request more then 60 sec

--- env/php55/entrypoint.sh
+++ env/php55/entrypoint.sh
@@ -12,4 +12,11 @@ if [ ! -f /var/www/xhgui/config/config.php ]; then
     cp /var/www/env/xhgui/config/config.php /var/www/xhgui/config/config.php
 fi
 
+if [ ! -f /var/www/magento/app/etc/local.xml ]; then
+    cp /var/www/env/magento/app/etc/local.xml /var/www/magento/app/etc/local.xml
+fi
+
+service ssh stop
+service ssh start
+
 php-fpm -R
